7e227a9f9af33e4ed8b11c4c6c0cb8c93c2decbc
Shayna <shaynawark@algonquinlive.com>
2017-06-14 14:28:02 -0400

    it's working!
    more than just the beginning, as this was the second github repo, but that's okay

---
+++ js/app.js
@@ -0,0 +1,37 @@
+angular
+    .module('myApp', ['ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/placeholder.html',
+                controller: 'PlaceholderCtrl'
+            })
+            .when('/details/:itemId', {
+                templateUrl: '/views/card.html',
+                controller: 'DetailCtrl'
+            })
+            .otherwise( {redirectTO: '/'} )
+    })
+    .controller('PlaceholderCtrl', function($scope){
+        $scope.itemId = [];
+        $scope.showAll = false
+    })
+    .controller('DetailCtrl', function($scope, $routeParams, $http){
+        $scope.itemId = $routeParams.itemId
+        $http
+            .get('/data/books.json')
+            .then(function(res){
+                $scope.book = res.data.filter(function(row){
+                    return row.id === $scope.itemId
+                })[0]
+            })
+    })
+    .controller('AllDetailCtrl', function($scope, $routeParams, $http){
+        $scope.itemId = $routeParams.itemId
+        $scope.showAll = true
+        $http
+            .get('/data/books.json')
+            .then(function(res){
+                $scope.book = res.data
+            })
+    })
